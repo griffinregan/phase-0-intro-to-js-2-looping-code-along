f02d4d02d8c4c2e527b4b2154ad5eadb210841b4
griffincregan <griffinregan814@gmail.com>
2022-05-20 13:13:15 -0500

    commit tree 573205b4040ff39c4c76210836b5a4c34a090b81

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+    console.log("namesArray", namesArray)
+    console.log("event", event)
+    const newArray= []
+    for (let i = 0; i < namesArray.length; i++) {
+        newArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+      }
+      return newArray
+    }
+    function countDown(number) {
+        while (number >= 0) {
+            console.log(number);
+            number--;
+          }
+    }
